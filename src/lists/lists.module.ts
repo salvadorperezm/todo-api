849a61d381f250a719aa0edd28ca837bfd685c1f
salvadorperezm <salvadorperezm115@gmail.com>
2023-01-29 21:25:09 -0600

    deleting unused lists service

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { List } from './entity/lists.entity';
-import { ListsService } from './service/lists.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([List])],
-  providers: [ListsService],
+
   exports: [TypeOrmModule],
 })
 export class ListsModule {}
